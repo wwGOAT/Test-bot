f8b393cb35250f5445ef27a851d553d8f0d5f485
wwGOAT <bekeldor101@gmail.com>
2024-04-07 20:33:37 +0500

    photo_bot

--- handlers/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import users_start
--- loader.py
+++ loader.py
@@ -1,10 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
 from main.config import TOKEN
-from database.db_connect import Database
 
 bot = Bot(token=TOKEN)
 storage = MemoryStorage()
-dp = Dispatcher(bot=bot, storage=storage)
-db = Database()
+dp = Dispatcher(bot, storage=storage)
